82fed1ae625319313da57b0787be8ea40d7b6d73
hand_made\user <34983498@naver.com>
2025-01-10 00:25:23 +0900

    viewcontroller 제품 상세 추가

--- ModeMe/src/main/java/com/example/Modeme/controller/ViewController.java
+++ ModeMe/src/main/java/com/example/Modeme/controller/ViewController.java
@@ -53,4 +53,10 @@ public class ViewController {
 		return "/MyPage/MyPage";
 	}
 	
+	//제품 상세
+	@GetMapping("/productDetail")
+	public String productDetail() {
+		return "/productDetail/productDetail";
+	}
+	
 }
