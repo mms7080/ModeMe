37f9ec3833c51df1159370706df9bfe503fb9e84
bum <mms7080@naver.com>
2025-01-09 22:29:30 +0900

    정민이바보

--- ModeMe/src/main/java/com/example/Modeme/controller/ViewController.java
+++ ModeMe/src/main/java/com/example/Modeme/controller/ViewController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ViewController {
 	
 	
-	//공지 테스트 ㅁㄴㅇㅁㄴㅇㄴㅇㄴ
+	//공지 정민이바보
 	@GetMapping("/notice") 
 	public String noticehome() {
 		return "/Notice/NoticeHome";
