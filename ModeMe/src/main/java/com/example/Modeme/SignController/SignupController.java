aa42db48a6f39bb40ae24cd7de5572ce6b964f41
Eunchae Kim <13rladmsco@gmail.com>
2025-01-14 00:28:54 +0900

    네이버 로그인 api 연결 반 성공..?

--- ModeMe/src/main/java/com/example/Modeme/SignController/SignupController.java
+++ ModeMe/src/main/java/com/example/Modeme/SignController/SignupController.java
@@ -2,6 +2,9 @@ package com.example.Modeme.SignController;
 //메롱
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class SignupController {
@@ -30,4 +33,19 @@ public class SignupController {
    public String modify() {
 	   return "/Sign/modify";
    }
+   
+   @RestController
+   @RequestMapping("/naver_callback")
+   public class NaverLoginController {
+
+       @GetMapping
+       public String handleNaverCallback(@RequestParam String code, @RequestParam String state) {
+           // 여기서 네이버 API를 호출하여 accessToken 및 사용자 정보를 가져옵니다.
+           // 예시: AccessToken 발급 및 사용자 정보 가져오기
+
+           // 로그인 후 원하는 페이지로 리디렉션
+           return "redirect:/main"; // "/main" 페이지로 리디렉션
+       }
+   }
+
 }
