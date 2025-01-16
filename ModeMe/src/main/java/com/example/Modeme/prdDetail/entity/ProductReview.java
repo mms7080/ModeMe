18b6467d750478826741d61e2585cc57276e050f
hand_made\user <34983498@naver.com>
2025-01-16 19:29:38 +0900

    productDetail 엔티티 드디어 완. 성.

--- ModeMe/src/main/java/com/example/Modeme/prdDetail/entity/ProductReview.java
+++ ModeMe/src/main/java/com/example/Modeme/prdDetail/entity/ProductReview.java
@@ -1,43 +1,48 @@
-//package com.example.Modeme.prdDetail.entity;
-//
-//import java.io.File;
-//import java.util.List;
-//
-//import com.example.Modeme.User.UserEntity.User;
-//
-//import jakarta.persistence.Column;
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.FetchType;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.JoinColumn;
-//import jakarta.persistence.ManyToOne;
-//import jakarta.persistence.SequenceGenerator;
-//import jakarta.persistence.Table;
-//
-//@Entity
-//@Table(name = "product_qa_table")
-//@SequenceGenerator(name = "ProductReviewSeq", sequenceName = "ProductReviewSeq", allocationSize = 1, initialValue = 1)
-//public class ProductReview {
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ProductReviewSeq")
-//	@Column(name = "review_id")
-//	private long id;
-//
-//	@ManyToOne(fetch = FetchType.LAZY)
-//	@JoinColumn(name = "product_board_id", nullable = false)
-//	private ProductBoard productBoard;
-//
-//	@ManyToOne(fetch = FetchType.LAZY)
-//	@JoinColumn(name = "user_id", nullable = false)
-//	private User user;
-//
-//	@Column(nullable = false, length = 50)
-//	private String title;
-//
-//	@Column(nullable = false, length = 500)
-//	private String content;
-//
-//	private List<File> images;
-//}
+package com.example.Modeme.prdDetail.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.Modeme.User.UserEntity.User;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "PRODUCT_REVIEW")
+@SequenceGenerator(name = "ReviewSeq", sequenceName = "ReviewSeq", allocationSize = 1, initialValue = 1)
+public class ProductReview {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ReviewSeq")
+	@Column(name = "review_id")
+	private long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "product_board_id", nullable = false)
+	private ProductBoard productBoard;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+	private User users;
+
+	@Column(nullable = false, length = 50)
+	private String title;
+
+	@Column(nullable = false, length = 500)
+	private String content;
+
+	@ElementCollection
+	@CollectionTable(name = "review_images", joinColumns = @JoinColumn(name = "review_id"))
+	@Column(name = "image_path", nullable = true)
+	private List<String> images = new ArrayList<>();
+}
--- ModeMe/src/main/java/com/example/Modeme/prdDetail/entity/Size.java
+++ ModeMe/src/main/java/com/example/Modeme/prdDetail/entity/Size.java
@@ -1,30 +1,34 @@
-//package com.example.Modeme.prdDetail.entity;
-//
-//import jakarta.persistence.Column;
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.SequenceGenerator;
-//import jakarta.persistence.Table;
-//
-//@Entity
-//@Table(name="product_size_table")
-//@SequenceGenerator(
-//        name = "ProductSizeSeq",
-//        sequenceName = "ProductSizeSeq",
-//        allocationSize = 1,
-//        initialValue = 1
-//)
-//public class Size {
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ProductSizeSeq")
-//	@Column(name="prd_size_id")
-//	private Long id;
-//	
-//	private String s;
-//	private String m;
-//	private String l;
-//	private String xl;
-//	private String freeSize;
-//}
+package com.example.Modeme.prdDetail.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "SIZE_TABLE")
+@SequenceGenerator(name = "SizeSeq", sequenceName = "SizeSeq", allocationSize = 1, initialValue = 1)
+public class Size {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SizeSeq")
+	@Column(name = "size_id")
+	private Long id;
+
+	@Column(name = "free_size")
+	private String freeSize;
+
+	@Column(name = "s")
+	private String s;
+
+	@Column(name = "m")
+	private String m;
+
+	@Column(name = "l")
+	private String l;
+
+	@Column(name = "xl")
+	private String xl;
+}
