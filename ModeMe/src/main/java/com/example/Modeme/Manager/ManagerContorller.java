30cc117dec8489f645ef394620215a1927b834a3
hand_made\user <34983498@naver.com>
2025-01-17 17:21:17 +0900

    Revert "관리자페이지 로그인유지"
    This reverts commit 1f3ca97a2f2bbd695487b4b17acb430fefcc6499.

--- ModeMe/src/main/java/com/example/Modeme/Manager/ManagerContorller.java
+++ ModeMe/src/main/java/com/example/Modeme/Manager/ManagerContorller.java
@@ -1,15 +1,11 @@
 package com.example.Modeme.Manager;
 
-import java.security.Principal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.example.Modeme.QnA.QnARepository.QnaRepository;
-import com.example.Modeme.User.UserDTO.Headerlogin;
 import com.example.Modeme.User.UserRepository.UserRepository;
 
 @Controller
@@ -19,13 +15,7 @@ public class ManagerContorller {
 
     @Autowired
     private QnaRepository qnaRepository;
-	@Autowired
-	Headerlogin keep; // 로그인 유지 재사용 Headerlogin 클래스
-	
-    @ModelAttribute //모든 매핑에 추가할 코드
-    public void addAttributes(Model model, Principal principal) {
-        keep.headerlogin(model, principal); //로그인 유지 
-    }
+    
     
     //관리자 메인
     @GetMapping("/managerMain")
