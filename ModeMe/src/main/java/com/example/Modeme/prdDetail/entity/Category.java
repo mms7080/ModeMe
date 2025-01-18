4faa6af3840511168a0c638cae1ee75276f73303
hand_made\user <34983498@naver.com>
2025-01-18 17:19:34 +0900

    Category 엔티티 수정

--- ModeMe/src/main/java/com/example/Modeme/prdDetail/entity/Category.java
+++ ModeMe/src/main/java/com/example/Modeme/prdDetail/entity/Category.java
@@ -37,6 +37,9 @@ public class Category {
 	@Column(name="category_id")
 	private Long id;
 	
+    @Column(name = "name", nullable = false, length = 50) // name 칼럼 추가
+    private String name;
+	
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "parent_id")
 	private Category parent;
