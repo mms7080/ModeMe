fd0caeea00618e97226647da7ff7944149d7e5d4
hand_made\user <34983498@naver.com>
2025-01-20 15:00:58 +0900

    엔티티 대규모 수정함,,ㅠ

--- ModeMe/src/main/java/com/example/Modeme/prdDetail/entity/Product.java
+++ ModeMe/src/main/java/com/example/Modeme/prdDetail/entity/Product.java
@@ -2,13 +2,9 @@ package com.example.Modeme.prdDetail.entity;
 
 import java.util.List;
 
-import com.example.Modeme.prdDetail.entity.ProductStock.SizeType;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,6 +14,10 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "PRODUCT")
@@ -27,6 +27,10 @@ import jakarta.persistence.Table;
         allocationSize = 1,
         initialValue = 1
 )
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Product {
 
     @Id
@@ -47,9 +51,8 @@ public class Product {
     @JoinColumn(name = "category_id")
     private Category category;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "product_size", nullable = false) // 필드 이름 변경
-    private SizeType productSize;
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<Fit> sizes;  // 수정: 단일 객체에서 List<Size>로 변경
 
     @Column(nullable = false)
     private String color;
