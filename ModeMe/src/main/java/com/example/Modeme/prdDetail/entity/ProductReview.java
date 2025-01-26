c909106df212459d3992a1aeb3727344e3dbb4df
hand_made\user <34983498@naver.com>
2025-01-26 19:31:34 +0900

    리뷰 수정

--- ModeMe/src/main/java/com/example/Modeme/prdDetail/entity/ProductReview.java
+++ ModeMe/src/main/java/com/example/Modeme/prdDetail/entity/ProductReview.java
@@ -1,14 +1,11 @@
 package com.example.Modeme.prdDetail.entity;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDateTime;
 
 import com.example.Modeme.Manager.Entity.AddItem;
 import com.example.Modeme.User.UserEntity.User;
 
-import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -16,9 +13,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
-import jakarta.persistence.Version;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -50,9 +47,11 @@ public class ProductReview {
 
 	@Column(nullable = false, length = 500)
 	private String content;
-
-//	@ElementCollection
-//	@CollectionTable(name = "review_images", joinColumns = @JoinColumn(name = "review_id"))
-//	@Column(name = "image_path", nullable = true)
-//	private List<String> images = new ArrayList<>();
+	
+	private LocalDateTime commentedTime;
+	
+    @PrePersist
+    protected void onCreate() {
+        this.commentedTime = LocalDateTime.now();
+    }
 }
