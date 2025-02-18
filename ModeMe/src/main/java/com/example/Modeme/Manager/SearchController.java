faae69b6887aac021dbe418b23d044c9a9d83d73
F-25\Administrator <mms7080@naver.com>
2025-02-18 21:42:07 +0900

    서치에도 변경된 이미지가 썸네일에 나오도록 수정 이거 이미지불러오는 모든 컨트롤러에 작성해야댐.. 나중에 공지함

--- ModeMe/src/main/java/com/example/Modeme/Manager/SearchController.java
+++ ModeMe/src/main/java/com/example/Modeme/Manager/SearchController.java
@@ -1,7 +1,9 @@
 package com.example.Modeme.Manager;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -9,12 +11,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.example.Modeme.Manager.Entity.AddItem;
+import com.example.Modeme.Manager.Entity.ProductImage;
+import com.example.Modeme.Manager.ManagerRepository.ProductImageRepository;
 import com.example.Modeme.Manager.ManagerService.AddItemService;
+
+
 
 @Controller
 public class SearchController {
 
 	private final AddItemService addItemService;
+	
+	@Autowired
+	private ProductImageRepository productImageRepository;
 
     public SearchController(AddItemService addItemService) {
         this.addItemService = addItemService;
@@ -23,6 +32,15 @@ public class SearchController {
     @GetMapping("/search")
     public String searchItems(@RequestParam String keyword, Model model) {
         List<AddItem> searchResults = addItemService.searchItems(keyword);
+        
+	    for (AddItem item : searchResults) {
+	        List<String> latestImages = productImageRepository.findByAddItemId(item.getId())
+	                                         .stream()
+	                                         .map(ProductImage::getImageUrl)
+	                                         .collect(Collectors.toList());
+	        item.setImageUrls(latestImages);  // 최신 이미지 적용
+	    }
+	    
         model.addAttribute("aList", searchResults);
         model.addAttribute("searchKeyword", keyword); // 검색어 추가
         model.addAttribute("resultCount", searchResults.size()); // 검색 결과 개수 추가
